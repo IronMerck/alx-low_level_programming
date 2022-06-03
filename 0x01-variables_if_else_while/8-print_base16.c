8066c2727288f1203119f8b2c6ed46be8a7f661b
BroCrown <bereketaklile@gmail.com>
2022-06-03 06:56:59 -0700

    00

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - Entry point
- * Return:0
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, y;
-	for (i = 0; i < 10; i++)
-	{
-		putchar (i + '0');
-	}
-	for (y = 'a'; y <= 'f'; y++)
-	{
-		putchar (y);
-	}
+	int numb;
+	char alphab;
 
+	for (numb = 48 ; numb <= 57 ; numb++)
+		putchar(numb);
+	for (alphab = 97 ; alphab <= 102 ; alphab++)
+		putchar(alphab);
 	putchar('\n');
 	return (0);
-
 }
