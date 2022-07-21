1ea0b88eb363c3b8e8289c7226ccb3218cb834cc
BroCrown <bereketaklile@gmail.com>
2022-07-21 07:32:16 -0700

    0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,9 @@
-#ifndef MAINN_H
-#define MAINN_H
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
 
-#define BYTE_LENGHT 8
-
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
